7f265f400a1a4eb94c76f15298052020052a7c18
fbn-mrtnz <username@users.noreply.github.com>
2018-01-06 13:05:02 +0000

    Part1 - Debugger Display Attribute

---
+++ TipsAndTrapsPartOne.Tests/DebuggerDisplayAttributeExample.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TipsAndTrapsPartOne._01_DebuggerDIsplayAttribute;
+
+namespace TipsAndTrapsPartOne.Tests
+{
+    [TestClass]
+    public class DebuggerDisplayAttributeExample
+    {
+        [TestMethod]
+        public void Wihtout()
+        {
+            var p = new PersonWithoutDebuggerDisplay()
+            {
+                AgeInYears = 50,
+                Name="Sarah"
+            };
+        }
+
+        [TestMethod]
+        public void With()
+        {
+            var p = new PersonWithDebuggerDisplay()
+            {
+                AgeInYears = 50,
+                Name = "Sarah"
+            };
+        }
+
+        // DebuggerDisplayAttribute can be applied to:
+        // Classes
+        // Delegates
+        // Structs
+        // Enums
+        // Fields
+        // Properties
+        // Assemblies
+    }
+}
---
+++ TipsAndTrapsPartOne/01_DebuggerDIsplayAttribute/PersonWithoutDebuggerDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+
+namespace TipsAndTrapsPartOne._01_DebuggerDIsplayAttribute
+{
+    public interface IPerson
+    {
+        int AgeInYears { get; set; }
+        string Name { get; set; }
+    }
+
+    public class PersonWithoutDebuggerDisplay : IPerson
+    {
+        public int AgeInYears { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return "This is overriden ToString()";
+        }
+    }
+
+    [DebuggerDisplay("This person is called {Name} and is {AgeInYears} years old")]
+    public class PersonWithDebuggerDisplay : IPerson
+    {
+        [DebuggerDisplay("{AgeInYears} years old")]
+        public int AgeInYears { get; set; }
+        public string Name { get; set; }
+    }
+
+}
