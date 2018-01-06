3228144c7ee0ba0457e9e29adebbb738682e7f23
fbn-mrtnz <username@users.noreply.github.com>
2018-01-06 17:15:58 +0000

    05 - Caller Info Attributes

---
+++ TipsAndTrapsPartOne.Tests/05_CallerInfoAttributes/CallerInfoAttributesExample.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using TipsAndTrapsPartOne._05_CallerInfoAttributes;
+
+namespace TipsAndTrapsPartOne.Tests._05_CallerInfoAttributes
+{
+    [TestClass]
+    public class CallerInfoAttributesExample
+    {
+        [TestMethod]
+        public void ExampleFooMethod()
+        {
+            var c = new CallerInfoAttributeDemo();
+
+            Debug.WriteLine(c.WhoCalledMe());
+            Debug.WriteLine(c.WhatFileCalledMe());
+            Debug.WriteLine(c.WhatLineCalledMe());
+        }
+    }
+}
---
+++ TipsAndTrapsPartOne/05_CallerInfoAttributes/CallerInfoAttributeDemo.cs
@@ -0,0 +1,60 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using TipsAndTrapsPartOne.Annotations;
+
+namespace TipsAndTrapsPartOne._05_CallerInfoAttributes
+{
+    public class CallerInfoAttributeDemo
+    {
+        public string WhoCalledMe([CallerMemberName] string callingMember = null)
+        {
+            return "I was called from member " + callingMember;
+        }
+
+        public string WhatFileCalledMe([CallerFilePath] string callingFile = null)
+        {
+            return "I was called by file " + callingFile;
+        }
+
+        public string WhatLineCalledMe([CallerLineNumber] int callingLineNum = 0)
+        {
+            return "I was called from line " + callingLineNum;
+        }
+    }
+
+    public class CallerInfoAttributeDemo2 : INotifyPropertyChanged
+    {
+        private int _age;
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                _age = value;
+                // Without [CallerMemberName] we have to pass a string of the prop name
+                // OnPropertyChanged("Age");
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
